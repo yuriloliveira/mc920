2c78e0e0057fd6249dd1fa585d2380fde2649578
yuriloliveira <yuri12194@gmail.com>
2020-10-13 17:03:42 -0300

    feat: support for all masks & filter function file.

--- prj1/main.py
+++ prj1/main.py
@@ -1,28 +1,16 @@
-import numpy as np
 import cv2
+import sys
+from masks import get_mask
+from filter import apply_filter
 
-def filter_pixel(img, pixel, mask):
-    x = pixel[0]
-    y = pixel[1]
-    offset_size = int(np.floor(mask.shape[0] / 2))
-    target_slice = img[x-offset_size:x+offset_size+1, y-offset_size:y+offset_size + 1]
-    return np.uint8(np.round(np.sum(target_slice* mask) / np.sum(mask)))
+filename = sys.argv[1] if len(sys.argv) == 2 else './images/lena.png'
+mask_id = sys.argv[2] if len(sys.argv) == 3 else 'h1'
 
-def apply_filter(img, mask):
-    # non-vectorized form, assuming mask of 3x3
-    # places zero into border pixels
-    result = np.zeros(img.shape, dtype=np.uint8)
-    for x in range(1, img.shape[0] - 1):
-        for y in range(1, img.shape[1] - 1):
-            result[x][y] = filter_pixel(img, (x, y), mask)
-    return result
+img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+mask = get_mask(mask_id)
+filtered_image = apply_filter(img, mask)
 
-img = cv2.imread('./images/lena.png', cv2.IMREAD_GRAYSCALE)
-
-h6 = (1/9) * np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-filtered_image = apply_filter(img, h6)
-print(filtered_image.shape)
 cv2.imshow('image', img)
-cv2.imshow('filtered image', filtered_image)
+cv2.imshow('filtered image (mask=' + mask_id + ')', filtered_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
