3239fd0e9853425b196950401af7968f6d4a42db
yuriloliveira <yuri12194@gmail.com>
2020-10-17 19:03:00 -0300

    refac: removes multiple masks support.

--- prj1/main.py
+++ prj1/main.py
@@ -3,25 +3,20 @@ import numpy as np
 import sys
 from argparse import ArgumentParser
 from masks import get_mask
-from filter_utils import show_filtered_image
+from filter_utils import show_filtered_image, filter_image, add_filters, show_src_and_filtered_image
 
 MASK_COUNT = 11
 
 parser = ArgumentParser('Applies mask to image', allow_abbrev=True)
 parser.add_argument('--image', '-i', help='image to be filtered', dest='image')
-parser.add_argument('--masks', '-m', help='mask(s) to be applied to the image. Available masks are h1..h11', nargs='+', default='h1', dest='masks')
+parser.add_argument('--mask', '-m', help='mask to be applied to the image. Available masks are h1..h11', default='h1', dest='mask')
 args = vars(parser.parse_args())
 
 filename = args['image']
-mask_ids = args['masks']
+mask_id = args['mask']
 
 img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-
-for mask_id in mask_ids:
-    try:
-        show_filtered_image(img, mask_id)
-    except Exception as e:
-        print("An error occurred: ", e)
+show_filtered_image(img, mask_id)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
