a0456aa9f141943ccb829f4dddc2304042963992
yuriloliveira <yuri12194@gmail.com>
2020-10-17 19:02:34 -0300

    feat: combination of filters.

--- prj1/filter_utils.py
+++ prj1/filter_utils.py
@@ -16,8 +16,34 @@ def plot_filtered_images(src_image, mask_ids):
 
 def show_filtered_image(src_image, mask_id):
     filtered_image = filter_image(src_image, mask_id)
+    show_src_and_filtered_image(src_image, filtered_image, 'source image / filtered image (mask = ' + mask_id + ')')
+
+    other_mask_id = 'h4' if mask_id == 'h3' else 'h3' if mask_id == 'h4' else None
+
+    if (other_mask_id):
+        print(other_mask_id)
+        other_filtered_image = filter_image(src_image, other_mask_id)
+        combined_filters_image = add_filters(filtered_image, other_filtered_image)
+        show_src_and_filtered_image(
+            src_image,
+            combined_filters_image,
+            'source image / combination of h3 and h4 masks'
+        )
+
+def show_src_and_filtered_image(src_image, filtered_image, title=''):
     result = np.hstack((src_image, filtered_image))
-    cv2.imshow('Source image / filtered image (mask = ' + mask_id + ')', result)
+    cv2.imshow(title, result) 
 
 def filter_image(src_image, mask_id, borderType=cv2.BORDER_REPLICATE):
     return cv2.filter2D(src_image, -1, get_mask(mask_id), borderType=borderType)
+
+def add_filters(img1, img2):
+    added_filer_image = (img1.astype(float)**2 + img2.astype(float)**2) ** (1/2)
+    return cv2.normalize(
+        added_filer_image,
+        None,
+        alpha=0,
+        beta=255,
+        norm_type=cv2.NORM_MINMAX,
+        dtype=cv2.CV_32F
+    ).astype(np.uint8)
