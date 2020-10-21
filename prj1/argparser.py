0284bbe9cb8290fa378a1f6cd2b308d1568fb232
yuriloliveira <yuri12194@gmail.com>
2020-10-21 18:35:31 -0300

    refac: removes arg parsing from main file.

---
+++ prj1/argparser.py
@@ -0,0 +1,32 @@
+from argparse import ArgumentParser
+
+class Parser:
+    __parser = ArgumentParser('Filters image applying mask')
+    __args = []
+
+    def __init__(self):
+        self.__parser.add_argument('--image', '-i',\
+            help='path to image which will be filtered',\
+            dest='image'
+        )
+        self.__parser.add_argument('--mask', '-m',\
+            help='mask to be applied to the image. Available masks are h1..h11',\
+            default='h1', dest='mask'
+        )
+        self.__parser.add_argument('--out', '-o',\
+            help='path to output of filtering (output will be an .png image)',\
+            dest='output_path'
+        )
+        self.__parser.add_argument('--outc', '-oc', \
+            help='path to output of combined images',\
+            dest='output_combined_path'
+        )
+        self.__parser.add_argument('--hflip', '-hf',\
+            help='if passed, flips the resulting image horizontally',\
+            dest='hflip',\
+            action='store_true'
+        )
+        self.__args = vars(self.__parser.parse_args())
+
+    def get_arg(self, arg_id):
+        return self.__args[arg_id]
--- prj1/image_filter.py
+++ prj1/image_filter.py
@@ -1,25 +1,19 @@
 import cv2
 import numpy as np
 import sys
-from argparse import ArgumentParser
+# from argparse import ArgumentParser
+from argparser import Parser
 from masks import get_mask
 from filter_utils import show_filtered_image, filter_image, add_filters, show_src_and_filtered_image
 
 MASK_COUNT = 11
 
-parser = ArgumentParser('Applies mask to image')
-parser.add_argument('--image', '-i', help='path to image which will be filtered', dest='image')
-parser.add_argument('--mask', '-m', help='mask to be applied to the image. Available masks are h1..h11', default='h1', dest='mask')
-parser.add_argument('--out', '-o', help='path to output of filtering (output will be an .png image)', dest='output_path')
-parser.add_argument('--outc', '-oc', help='path to output of combined images', dest='output_combined_path')
-parser.add_argument('--hflip', '-hf', help='if passed, flips the resulting image horizontally', dest='hflip', action='store_true')
-args = vars(parser.parse_args())
-
-filename = args['image']
-mask_id = args['mask']
-output_filename = args['output_path']
-output_combined_filename = args['output_combined_path']
-hflip = args['hflip']
+parser = Parser()
+filename = parser.get_arg('image')
+mask_id = parser.get_arg('mask')
+output_filename = parser.get_arg('output_path')
+output_combined_filename = parser.get_arg('output_combined_path')
+hflip = parser.get_arg('hflip')
 
 img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 flip_axis = 1 if hflip else None
