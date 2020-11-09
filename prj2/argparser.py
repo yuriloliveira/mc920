cc1ec881ce7ddcd90d5ebd67b56175ac1b916b4c
yuriloliveira <yuri12194@gmail.com>
2020-11-09 20:26:01 -0300

    feat: adds image argument support

---
+++ prj2/argparser.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+
+class Parser:
+    __parser = ArgumentParser('Filters image applying mask')
+    __args = []
+
+    def __init__(self):
+        self.__parser.add_argument('--image', '-i',\
+            help='path to image which will be filtered',\
+            dest='image'
+        )
+        self.__parser.add_argument('--out', '-o',\
+            help='path to output of filtering (output will be an .png image)',\
+            dest='output_path'
+        )
+        self.__parser.add_argument('--skip-image-show', '-ss',\
+            help="if passed, a window showing the source image and the filtered won't be open",\
+            dest='skip_image_show',\
+            action='store_true'
+        )
+        self.__args = vars(self.__parser.parse_args())
+
+    def get_arg(self, arg_id):
+        return self.__args[arg_id]
--- prj2/halftoning.py
+++ prj2/halftoning.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np 
 from utils import floyd_stein
+from argparser import Parser
 
-img = cv2.imread('images/baboon.png', cv2.IMREAD_COLOR)
+args = Parser()
+img_path = args.get_arg('image')
+img_path = 'images/peppers.png' if img_path is None else img_path
+img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 res_img = floyd_stein(np.array(img))
-
 cv2.imshow('source image', img)
 cv2.imshow('resulting image', res_img)
 
