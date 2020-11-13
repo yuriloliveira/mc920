583f8539c4b1a65559e72eb187b7d08a2fab0d06
yuriloliveira <yuri12194@gmail.com>
2020-11-12 21:37:09 -0300

    feat: vectorizes pixel treatment functions. Removes report commets.

--- prj2/utils.py
+++ prj2/utils.py
@@ -13,17 +13,12 @@ def halftoning(img, edist_id='a', sweep_mode='default'):
     for row in range(0, height):
         row_range_min = 0 if sweep_mode != 'alternate' or row % 2 == 0 else width -1
         row_range_max = width if row_range_min == 0 else -1
-        # IMPORTANTE: Comentar no relatório necessidade de utilizar step para alternado funcionar
         step = 1 if row_range_min == 0 else -1
         for col in range(row_range_min, row_range_max, step):
             result[row][col] = convert_pixel(img[row][col])
             err_rgb = get_error_rgb(img[row][col], result[row][col]) 
             apply_err(img, (row, col), err_rgb, error_distribution)
 
-    #####################################################################
-    ### IMPORTANTE: Comparar sem normalize/com normalize no relatório ###
-    ### return result                                                 ###
-    #####################################################################
     return cv2.normalize(
         result,
         None,
@@ -34,20 +29,13 @@ def halftoning(img, edist_id='a', sweep_mode='default'):
     ).astype(np.uint8)
 
 def convert_pixel(curr_pixel):
-    [b, g, r] = curr_pixel
-    return [
-        0 if b < 128 else 1,
-        0 if g < 128 else 1,
-        0 if r < 128 else 1
-    ]
+    pi = np.array(curr_pixel)
+    pi[curr_pixel < 128] = 0
+    pi[curr_pixel >= 128] = 1
+    return pi
 
 def get_error_rgb(old_pi, new_pi, pi_length=3):
-    result = np.zeros((3))
-    for i in range(0, pi_length):
-        # IMPORTANTE: Comparar com indexização errada no pixel no relatório
-        # result[i] = old_pi[0] - new_pi[0] * 255
-        result[i] = old_pi[i] - new_pi[i] * 255
-    return result
+    return old_pi - new_pi * 255
 
 def apply_err(img, pi_pos, error_rgb, edist):
     (w, h) = edist.shape
@@ -56,7 +44,7 @@ def apply_err(img, pi_pos, error_rgb, edist):
 
     for i in range(0, w):
         for j in range(0, h):
-            target_x = x + i - half_w # Abordar esta decisão no relatório (limitação)
+            target_x = x + i - half_w
             target_y = y + j
             if target_x < 0 or target_y < 0 or\
                target_x >= img.shape[0] or target_y >= img.shape[1]:
